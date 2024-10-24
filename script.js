500a71c743218a6df494ac17559f58b927b3ac4d
haipon <143789467+haipon@users.noreply.github.com>
2024-10-24 13:16:07 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.image-card').forEach(card => {
-    card.addEventListener('click', function() {
-        const target = document.querySelector(this.getAttribute('data-target'));
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
-});
