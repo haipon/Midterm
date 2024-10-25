5d60c19f2e85079226f6e5d801918c876a19af82
Arini Nur Azizah <155711234+sun7ren@users.noreply.github.com>
2024-10-25 21:38:14 +0700

    Delete admin directory

--- admin/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE products (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    description TEXT,
-    price DECIMAL(10, 2),
-    image VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
